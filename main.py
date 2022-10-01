b8051df3c0c1b78ee1be38fa4a640433888dea9d
magnusknutas <magnusknutas@gmail.com>
2022-10-01 23:09:55 +0200

    adding files

---
+++ main.py
@@ -0,0 +1,60 @@
+"""
+Main start script
+"""
+import asyncio
+import logging
+import sys
+from asyncio import AbstractEventLoop
+from threading import Thread
+from typing import Any, List, Tuple, Union
+
+from conscious import Conscious
+from senses import KeyBoard
+
+multi: List[Any] = [Conscious, KeyBoard]
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        # logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+    ],
+)
+
+
+async def work(cls: Union[Conscious, KeyBoard]):
+    """
+    Work starter
+    :param cls:
+    :return:
+    """
+    while True:
+        await cls.loop()
+
+
+def worker(cls: Union[Conscious, KeyBoard]) -> None:
+    """
+    Worker
+    :param cls: Worker class
+    :return:
+    """
+    new_loop: AbstractEventLoop = asyncio.new_event_loop()
+    asyncio.set_event_loop(new_loop)
+    new_loop.run_until_complete(work(cls))
+
+
+def main() -> None:
+    """
+    main function
+    :return:
+    """
+    threads: Tuple = ()
+    for _cls in multi:
+        thread: Thread = Thread(target=worker, args=[_cls])
+        threads += (thread,)
+        thread.start()
+
+
+if __name__ == "__main__":
+    main()
